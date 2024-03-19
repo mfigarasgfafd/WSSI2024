1843fc520e7b41d7178fd4ed376cd75d739c3683
Mateusz Figarski <100209142+mfigarasgfafd@users.noreply.github.com>
2024-03-19 17:32:54 +0000

    zajecia

---
+++ LB_2/LB_2_zad1_dodatek.pl
@@ -0,0 +1,55 @@
+% osoby
+osoba(adam).
+osoba(ewa).
+osoba(kain).
+osoba(abel).
+osoba(set).
+osoba(zuzanna).
+osoba(jan).
+osoba(anna).
+
+% mężczyźni
+mezczyzna(adam).
+mezczyzna(kain).
+mezczyzna(abel).
+mezczyzna(set).
+mezczyzna(jan).
+
+% rodzice
+rodzic(adam, kain).
+rodzic(adam, abel).
+rodzic(adam, set).
+rodzic(ewa, kain).
+rodzic(ewa, abel).
+rodzic(ewa, set).
+rodzic(jan, zuzanna).
+rodzic(anna, zuzanna).
+
+
+kobieta(X) :- osoba(X), \+ mezczyzna(X).
+
+ojciec(X,Y) :- rodzic(X,Y), mezczyzna(X).
+
+matka(X,Y) :- rodzic(X,Y), kobieta(X).
+
+corka(X,Y) :- rodzic(Y,X), kobieta(X).
+
+brat_rodzony(X,Y) :- mezczyzna(X), osoba(Y), rodzic(Z,X), rodzic(Z,Y), X \= Y.
+
+brat_przyrodni(X,Y) :- mezczyzna(X), osoba(Y), rodzic(Z,X), rodzic(Z,Y), rodzic(W,X), rodzic(W,Y), Z \= W, X \= Y.
+
+kuzyn(X,Y) :- rodzic(Z,X), rodzic(W,Y), brat_rodzony(Z,W).
+
+dziadek_od_strony_ojca(X,Y) :- mezczyzna(X), ojciec(X,Z), ojciec(Z,Y).
+
+dziadek_od_strony_matki(X,Y) :- mezczyzna(X), matka(Z,Y), rodzic(X,Z).
+
+dziadek(X,Y) :- mezczyzna(X), rodzic(X,Z), rodzic(Z,Y).
+
+babcia(X,Y) :- kobieta(X), rodzic(X,Z), rodzic(Z,Y).
+
+wnuczka(X,Y) :- kobieta(Y), rodzic(Z,Y), rodzic(X,Z).
+
+przodek_do2pokolenia_wstecz(X,Y) :- rodzic(X,Z), rodzic(Z,Y).
+
+przodek_do3pokolenia_wstecz(X,Y) :- rodzic(X,A), rodzic(A,Z), rodzic(Z,Y).
