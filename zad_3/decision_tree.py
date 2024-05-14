3b21bd2b0fc9a5de28f8f8ccda1f6975bea47751
Mateusz Figarski <100209142+mfigarasgfafd@users.noreply.github.com>
2024-05-14 12:52:56 +0000

    final (acc 1.0)

--- zad_3/decision_tree.py
+++ zad_3/decision_tree.py
@@ -20,68 +20,65 @@ class DecisionTree:
         self.root = None
 
     def fit(self, X, y):
-        self.n_features = X.shape[1] if not self.n_features else min(self.n_features, X.shape[1])
+        self.n_classes_ = len(set(y))
+        self.n_features_ = X.shape[1] if not self.n_features else min(self.n_features, X.shape[1])
+        if self.n_features:
+            self.feat_idxs = np.random.choice(self.n_features_, self.n_features, replace=False)
+        else:
+            self.feat_idxs = range(self.n_features_)
         self.root = self._grow_tree(X, y)
+
 
     def _grow_tree(self, X, y, depth=0):
         n_samples, n_features = X.shape
         n_labels = len(np.unique(y))
 
         if (depth >= self.max_depth or n_labels == 1 or n_samples < self.min_samples_split):
-            leaf_value = Counter(y).most_common(1)[0][0]
+            leaf_value = self.most_common_helper(y)
             return Node(value=leaf_value)
 
         feat_idxs = np.random.choice(n_features, self.n_features, replace=False)
+        
+        best_feat, best_thresh = self._best_split(X, y, self.feat_idxs)
 
-        best_feat, best_thresh = self._best_split(X, y, feat_idxs)
         left_idxs, right_idxs = self._split(X[:, best_feat], best_thresh)
-
-        if len(left_idxs) == 0 or len(right_idxs) == 0:
-            leaf_value = Counter(y).most_common(1)[0][0]
-            return Node(value=leaf_value)
-
-        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth + 1)
-        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth + 1)
-        return Node(best_feat, best_thresh, left, right)
-
+        left = self._grow_tree(X[left_idxs, :], y[left_idxs], depth+1)
+        right = self._grow_tree(X[right_idxs, :], y[right_idxs], depth+1)
+        return Node(feature=best_feat, threshold=best_thresh, left=left, right=right)
 
     def _best_split(self, X, y, feat_idxs):
-        best_entropy = float('inf')
-        best_feat, best_thresh = None, None
-
+        best_gain = -1
+        split_idx, split_thresh = None, None
         for feat_idx in feat_idxs:
             X_column = X[:, feat_idx]
             thresholds = np.unique(X_column)
-
             for threshold in thresholds:
-                entropy_val = self._information_gain(y, X_column, threshold)
+                gain = self._information_gain(y, X_column, threshold)
+                if gain > best_gain:
+                    best_gain = gain
+                    split_idx = feat_idx
+                    split_thresh = threshold
+        return split_idx, split_thresh
 
-                if entropy_val < best_entropy:
-                    best_entropy = entropy_val
-                    best_feat = feat_idx
-                    best_thresh = threshold
-
-        return best_feat, best_thresh
-
-    def _information_gain(self, y, X_column, threshold):
+    def _information_gain(self, y, X_column, split_thresh):
         parent_entropy = self._entropy(y)
-        left_idxs, right_idxs = self._split(X_column, threshold)
+
+        left_idxs, right_idxs = self._split(X_column, split_thresh)
 
         if len(left_idxs) == 0 or len(right_idxs) == 0:
             return 0
 
-        left_entropy = self._entropy(y[left_idxs])
-        right_entropy = self._entropy(y[right_idxs])
-
         n = len(y)
-        left_weight = len(left_idxs) / n
-        right_weight = len(right_idxs) / n
+        n_l, n_r = len(left_idxs), len(right_idxs)
+        e_l, e_r = self._entropy(y[left_idxs]), self._entropy(y[right_idxs])
+        child_entropy = (n_l / n) * e_l + (n_r / n) * e_r
 
-        return parent_entropy - (left_weight * left_entropy + right_weight * right_entropy)
+        ig = parent_entropy - child_entropy
+        return ig
 
-    def _split(self, X_column, threshold):
-        left_idxs = np.argwhere(X_column <= threshold).flatten()
-        right_idxs = np.argwhere(X_column > threshold).flatten()
+    def _split(self, X_column, split_thresh):
+        left_idxs = np.argwhere(X_column < split_thresh).flatten()
+        right_idxs = np.argwhere(X_column >= split_thresh).flatten()
         return left_idxs, right_idxs
 
     def _entropy(self, y):
@@ -92,10 +89,14 @@ class DecisionTree:
     def _traverse_tree(self, x, node):
         if node.is_leaf_node():
             return node.value
-
-        if x[node.feature] <= node.threshold:
+        if x[node.feature] < node.threshold:
             return self._traverse_tree(x, node.left)
         return self._traverse_tree(x, node.right)
 
     def predict(self, X):
         return np.array([self._traverse_tree(x, self.root) for x in X])
+
+    def most_common_helper(self, y):
+        counter = Counter(y)
+        most_common = counter.most_common(1)[0][0]
+        return most_common
